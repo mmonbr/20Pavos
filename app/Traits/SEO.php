a39c3eec5e266f4f43b8a7f812e6d901ed1cf304
Manuel Montenegro <manuel.monbr@gmail.com>
2016-02-01 13:05:48 +0100

    Open Graph Test

--- app/Traits/SEO.php
+++ app/Traits/SEO.php
@@ -10,7 +10,7 @@ trait SEO {
         \OpenGraph::setTitle($product->name);
         \OpenGraph::setDescription($product->description);
         \OpenGraph::setUrl(route('products.show', [$product->slug]));
-        \OpenGraph::addImage(cdn_file($product->image_url));
+        \OpenGraph::addImage(http_file($product->image_url));
         \OpenGraph::addProperty('image:width', '300');
         \OpenGraph::addProperty('image:height', '250');
     }
