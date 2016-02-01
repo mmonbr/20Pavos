38ab854c310e3e4d87459128025a9120ef2044f8
Manuel Montenegro <manuel.monbr@gmail.com>
2016-02-01 12:00:33 +0100

    HTTPS

--- resources/views/frontend/products/partials/_share.blade.php
+++ resources/views/frontend/products/partials/_share.blade.php
@@ -25,21 +25,21 @@
             <a rel="nofollow" target="_blank" class="new_window"
                href="http://facebook.com/sharer.php?u={{ $url }}">
                 <img class="ShareButton__item__image"
-                     src="{{ asset('img/share/facebook.png') }}"
+                     src="{{ secure_asset('img/share/facebook.png') }}"
                      alt="Compartir en facebook"></a>
         </li>
         <li class="ShareButton__item">
             <a rel="nofollow" target="_blank" class="new_window"
                href="http://twitter.com/share?url={{ $url }}&text={{ $name }}&via={{ config('settings.twitter.via') }}">
                 <img class="ShareButton__item__image"
-                     src="{{ asset('img/share/twitter.png') }}"
+                     src="{{ secure_asset('img/share/twitter.png') }}"
                      alt="Compartir en twitter"></a>
         </li>
         <li class="ShareButton__item">
             <a rel="nofollow" target="_blank" class="new_window"
                href="http://pinterest.com/pin/create/button/?url={{ $url }}&media=&description={{ $description }}&media={{ $media }}">
                 <img class="ShareButton__item__image"
-                     src="{{ asset('img/share/pinterest.png') }}"
+                     src="{{ secure_asset('img/share/pinterest.png') }}"
                      alt="Compartir en pinterest"></a>
         </li>
     </ul>
