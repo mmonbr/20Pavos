7217eb789864e64bd098b479b494254491c49b68
Manuel Montenegro <manuel.monbr@gmail.com>
2016-02-06 16:57:10 +0100

    Sidebar partial

---
+++ resources/views/frontend/layouts/partials/_header_content.blade.php
@@ -0,0 +1,53 @@
+<div class="ui grid container">
+    <div class="computer only row">
+        <a href="{{ route('home') }}" class="item">
+            <img src="{{ asset('img/logo.png') }}" class="ui small image">
+        </a>
+        <div class="ui dropdown item">
+            <i class="big sidebar icon"></i>
+            Explorar
+            <div class="menu">
+                <a href="#" class="item">Novedades</a>
+                <a href="{{ route('home') }}?max_price=10" class="item">Menos de 10€</a>
+                <a href="#" class="item">Categoría 1</a>
+                <a href="#" class="item">Categoría 2</a>
+                <a href="#" class="item">Categoría 3</a>
+                <a href="#" class="item">Categoría 4</a>
+                <a href="#" class="item">Contacto</a>
+            </div>
+        </div>
+
+        <div class="right menu">
+            @if(auth()->user())
+                <div class="ui dropdown item">
+                    <img class="ui avatar image" src="{{ Gravatar::src(auth()->user()->email, 28) }}">
+                    {{ '@' . auth()->user()->username }}<i class="dropdown icon"></i>
+
+                    <div class="menu">
+                        <a class="item"><i class="gift icon"></i> Lista de regalos</a>
+                        <a href="{{ route('users.edit') }}" class="item"><i
+                                    class="wrench icon"></i> Configuración</a>
+                        <a href="{{ route('auth.logout') }}" class="item"><i class="sign out icon"></i> Logout</a>
+                    </div>
+                </div>
+            @else
+                <a href="{{ route('auth.registration') }}" class="item"><i class="big signup icon"></i> Registro</a>
+                <a href="{{ route('auth.form') }}" class="item"><i class="big sign in icon"></i> Login</a>
+            @endif
+            <div class="item">
+                @include('frontend.layouts.partials._search', ['prompt' => true, 'placeholder' => '¿Buscas algo?'])
+            </div>
+        </div>
+    </div>
+    <div class="center aligned  tablet and mobile only row">
+        <a class="item toggle button">
+            <i class="fitted big sidebar icon"></i>
+        </a>
+
+        <div class="right menu">
+            <a href="{{ route('home') }}" class="item">
+                <img src="{{ asset('img/logo.png') }}" class="ui small image">
+            </a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/frontend/layouts/partials/_sidebar_menu.blade.php
@@ -0,0 +1,26 @@
+<div class="ui left vertical menu sidebar">
+    <a href="{{ route('home') }}" class="item"><i class="block home icon"></i> Inicio</a>
+    <a href="{{ route('products.latest') }}" class="item"><i class="block bullhorn icon"></i> Novedades</a>
+    <a href="{{ route('products.popular') }}" class="item"><i class="block heart icon"></i> Populares</a>
+    <a href="{{ route('products.cheap') }}" class="item"><i class="block euro icon"></i> Baratos</a>
+    @if(auth()->user())
+        <div class="ui dropdown item">
+            <img class="ui avatar image" src="{{ Gravatar::src(auth()->user()->email, 28) }}">
+            {{ '@' . auth()->user()->username }}<i class="dropdown icon"></i>
+
+            <div class="menu">
+                <a class="item"><i class="gift icon"></i> Lista de regalos</a>
+                <a href="{{ route('users.edit') }}" class="item"><i
+                            class="wrench icon"></i> Configuración</a>
+                <a href="{{ route('auth.logout') }}" class="item"><i class="sign out icon"></i> Logout</a>
+            </div>
+        </div>
+    @else
+        <a href="{{ route('auth.registration') }}" class="item"><i class="block signup icon"></i> Registro</a>
+        <a href="{{ route('auth.form') }}" class="item"><i class="block sign in icon"></i> Login</a>
+    @endif
+    <a href="{{ route('contact.form') }}" class="item"><i class="block envelope icon"></i>Contacto</a>
+    <div class="item">
+        @include('frontend.layouts.partials._search', ['prompt' => true, 'placeholder' => '¿Buscas algo?'])
+    </div>
+</div>
