f096a6bebdbe4b929f13456eef3e7d7ca6922f16
Manuel Montenegro <manuel.monbr@gmail.com>
2016-02-10 12:50:47 +0100

    Remove Zanox header verification

--- resources/views/frontend/layouts/partials/_head.blade.php
+++ resources/views/frontend/layouts/partials/_head.blade.php
@@ -1,5 +1,3 @@
-<meta name="verification" content="1fd56f06a89cc190f618e9ad862bad84" />
-
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
