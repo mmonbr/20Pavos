595961c1d5afc20960d7483f5f2afe18c140819b
Manuel Montenegro <manuel.monbr@gmail.com>
2016-02-10 12:47:53 +0100

    Add Zanox verification (https) fix

--- resources/views/frontend/layouts/partials/_head.blade.php
+++ resources/views/frontend/layouts/partials/_head.blade.php
@@ -1,3 +1,5 @@
+<meta name="verification" content="1fd56f06a89cc190f618e9ad862bad84" />
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,6 +10,3 @@
 
 <!-- Styles -->
 <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
-
-<!-- Zanox Verification -->
-<meta name="verification" content="1fd56f06a89cc190f618e9ad862bad84" />
