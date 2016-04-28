909c93cf33ef0340a14b7669f2ec1def8493a94f
Manuel Montenegro <manuel.montenegro@sngular.team>
2016-04-28 07:55:58 +0200

    Fix gitano para que no pete al intentar registrar un usuario social que ya estaba registrado de forma normal

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -17,6 +17,15 @@ class SocialAuthController extends Controller
     {
         $user = Socialite::driver('facebook')->user();
 
+        if (User::where('email', '=', $user->email)->first()) {
+            alert()
+                ->success('La dirección de email asociada al perfil social con el que estás intentando iniciar sesión ya se encuentra en nuestra base de datos.',
+                    'Oops.')
+                ->persistent('OK');
+            
+            return redirect(route('home'));
+        }
+
         $authUser = $this->findOrCreateUser($user, 'Facebook');
 
         auth()->login($authUser, true);
@@ -30,10 +39,11 @@ class SocialAuthController extends Controller
             return $authUser;
         }
 
-        alert()->success('¡Ya estás logeado! Te hemos asignado un nombre aleatorio pero, por favor, no te quedes con ese. Cámbialo por uno que te pegue más y escoge una contraseña para tu cuenta.', '¡Bienvenido!')->persistent('OK');
+        alert()->success('¡Ya estás logeado! Te hemos asignado un nombre aleatorio pero, por favor, no te quedes con ese. Cámbialo por uno que te pegue más y escoge una contraseña para tu cuenta.',
+            '¡Bienvenido!')->persistent('OK');
 
         return User::create([
-            'username'    => 'usuario_'.str_random(5),
+            'username'    => 'usuario_' . str_random(5),
             'email'       => $socialUser->email,
             'provider'    => $provider,
             'provider_id' => $socialUser->id,
