70879b14648a4f27e01b1da5eb41b4f962432220
Manuel Montenegro <manuel.monbr@gmail.com>
2016-05-04 10:01:45 +0200

    GA through CloudFlare

--- resources/views/frontend/layouts/partials/_scripts.blade.php
+++ resources/views/frontend/layouts/partials/_scripts.blade.php
@@ -5,9 +5,9 @@
 
 <script type="text/javascript">
     WebFontConfig = {
-        google: { families: [ 'Lato:400,400italic,700,700italic:latin' ] }
+        google: {families: ['Lato:400,400italic,700,700italic:latin']}
     };
-    (function() {
+    (function () {
         var wf = document.createElement('script');
         wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
                 '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
@@ -15,16 +15,7 @@
         wf.async = 'true';
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(wf, s);
-    })(); </script>
-
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-72528411-1', 'auto');
-    ga('send', 'pageview');
+    })();
 </script>
 
 @include('frontend.layouts.partials._cookies')
