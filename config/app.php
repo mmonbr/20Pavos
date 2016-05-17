07acce81754a5e51eb0ae7a2e314853259cd8f2c
Manuel Montenegro <manuel.monbr@gmail.com>
2016-05-17 15:35:05 +0200

    Improved Laravel DebugBar

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         GrahamCampbell\Flysystem\FlysystemServiceProvider::class,
         Yajra\Datatables\DatatablesServiceProvider::class,
         Spatie\LaravelAnalytics\LaravelAnalyticsServiceProvider::class,
-        Spatie\MediaLibrary\MediaLibraryServiceProvider::class
+        Spatie\MediaLibrary\MediaLibraryServiceProvider::class,
+        Fitztrev\QueryTracer\Providers\QueryTracerServiceProvider::class
 
     ],
 
