eac2860a320966976085d172236e43a7f36908a2
Manuel Montenegro <manuel.monbr@gmail.com>
2016-06-19 22:45:11 +0200

    Cleanup

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers\Backend;
 
-use Cache;
 use App\User;
-use LaravelAnalytics;
 use App\Products\Product;
 use Illuminate\Http\Request;
-use App\Utilities\SubscribersCache;
 use App\Http\Controllers\Controller;
 
 class BackendController extends Controller
