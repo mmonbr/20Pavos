bdbb53e96efeb26ffad99ec141f3b3ef80fce6ee
Manuel Montenegro <manuel.monbr@gmail.com>
2016-06-19 22:45:02 +0200

    Fix categories

--- resources/views/frontend/products/partials/_product_small.blade.php
+++ resources/views/frontend/products/partials/_product_small.blade.php
@@ -2,7 +2,7 @@
     <a class="Product__title Product__title--small" href="{{ route('products.show', $product->slug) }}">{{ $product->name }}</a>
 
     <div class="Product__media">
-        <a href="{{ $product->provider->link() }}" target="_blank">
+        <a href="{{ $product->link }}" target="_blank">
             <img class="Product__media__image" src="{{ $product->image() }}" alt="{{ $product->slug }}">
         </a>
     </div>
